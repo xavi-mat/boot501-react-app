988b64f8d1ddd6e945e274d5ebca001c2bfd73bc
Xavimat <xaviermatoses@gmail.com>
2022-06-09 18:06:20 +0200

    Change state in class

--- src/components/HomeClass/HomeClass.jsx
+++ src/components/HomeClass/HomeClass.jsx
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import jochibi from '../../assets/jochibi.jpg'
 
 export class HomeClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { chibi: false };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.setState({chibi: !this.state.chibi});
+    }
     render() {
         const bioLine = this.props.bioItems.map((line, num) => <p key={num}>{line}</p>)
+        const chibi = this.state.chibi;
         return (
             <div className="container row">
                 <div className="col-md-6 mt-3">
                     <h4>Bio</h4>
                     {bioLine}
                 </div>
-                <div className="col-md-6">
-                    Image in public<br/>
-                    <img className="img-fluid rounded" src={'./assets/' + this.props.img + '.jpg'} alt="careto" />
-                    Image in src<br/>
-                    <img src={jochibi} alt=""/>
+                <div className="col-md-6 text-center">
+                    <button className='btn btn-primary m-2 w-100' onClick={this.handleClick}>
+                        {chibi ? 'Serious' : 'Chibi' } image
+                    </button>
+                    <div className={chibi ? 'd-none' : ''}>
+                        Image in public<br />
+                        <img className="img-fluid rounded" src={'./assets/' + this.props.img + '.jpg'} alt="careto" />
+                    </div>
+                    <div className={chibi ? '' : 'd-none'}>
+                        Image in src<br />
+                        <img src={jochibi} alt="" />
+                    </div>
                 </div>
             </div>
         );
